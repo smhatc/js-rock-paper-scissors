0b8f110e8c9191111ae99fb50cf9516145ed877f
Sadeq Talaq <165420981+smhatc@users.noreply.github.com>
2025-06-29 13:10:17 +0300

    Simplified player choice logic

--- js/app.js
+++ js/app.js
@@ -23,22 +23,10 @@ function getComputerChoice() {
         return choices[computerChoiceIndex];
 }
 
-// Get the player's choice from the "choices" array (rock, paper, scissors) based on the clicked element
-function getPlayerChoice(event) {
-        // Returning the appropriate choice from the array based on the ID of the clicked element
-        if (event.target.id === "rock") {
-                return choices[choices.indexOf("rock")];
-        } else if (event.target.id === "paper") {
-                return choices[choices.indexOf("paper")];
-        } else if (event.target.id === "scissors") {
-                return choices[choices.indexOf("scissors")];
-        }
-}
-
 // Updates UI/HTML directly
 function render() {
         // Update the container paragraph element with the result
-        resultDisplayElement.textContent = `Computer chose ${computerChoice} and you chose ${playerChoice}`;
+        resultDisplayElement.textContent = `Computer chose ${computerChoice} and you chose ${playerChoice}.`;
 }
 
 // Initialize the game state
@@ -46,7 +34,7 @@ function play(event) {
         // Set the "computerChoice" variables's value
         computerChoice = getComputerChoice();
         // Set the "playerChoice" variables's value
-        playerChoice = getPlayerChoice(event);
+        playerChoice = event.target.id;
         // After updating the state, display the HTML
         render();
 }
